f9af30a14dc90b64b4e66fba0f0a03b951619d74
BrunoMarques <brunelleshi@gmail.com>
2023-04-21 17:47:15 -0300

    Post de artigos

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,33 @@
 class ArticlesController < ApplicationController
+    #id do artigo
     def index
         @articles = Article.all    
     end
 
+    #abre o artigo
     def show
         @article = Article.find(params[:id])
     end
+
+    #cria um artigo
+    def new
+        @article = Article.new
+    end
+
+    #salva o novo artigo
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            redirect_to @article
+        else
+            render :new
+        end
+    end
+
+    private
+    #verifica se o artigo esta dentro das regras para ser postado
+    def article_params
+        params.require(:article).permit(:title, :body)      
+    end
+
 end
