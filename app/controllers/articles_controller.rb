024b7988f87e0943456114ee4a8d6c84ef3576f8
BrunoMarques <brunelleshi@gmail.com>
2023-04-23 13:46:38 -0300

    Filtro de categorias

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,15 @@
 class ArticlesController < ApplicationController
+    include Paginable 
+
     before_action :set_article, only: %i[edit update show destroy]
     before_action :authenticate_user!, except: %i[index show]
     
     def index
-        @highlights = Article.desc_order.first(3)
-
-        current_page = (params[:page] || 1).to_i
+        category = Category.find_by_name(params[:category]) if params[:category].present?
+        @highlights = Article.filter_by_category(category).desc_order.first(3)
         highlights_ids = @highlights.pluck(:id).join(',')
-        @articles = Article.without_highlights(highlights_ids).desc_order.page(current_page) 
+        @articles = Article.without_highlights(highlights_ids).filter_by_category(category).desc_order.page(current_page) 
+        @categories = Category.sorted
     end
 
     #abre o artigo
