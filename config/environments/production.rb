ad10120100efa61a19cbe48a73b867ba0377b527
BrunoMarques <brunelleshi@gmail.com>
2023-04-24 16:05:02 -0300

    Domínio atualizado

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   # Email
-  config.action_mailer.default_url_options = { host: 'blogtyco.' }
+  config.action_mailer.default_url_options = { host: 'blogtyco-production.up.railway.app' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_setting ={
     address: 'smtp.gmail.com',
