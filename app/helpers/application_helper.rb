b17c9f8b2398055c7b1026f746dbc4ae5df80e7e
BrunoMarques <brunelleshi@gmail.com>
2023-04-25 13:55:23 -0300

    Otimizações e updates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
     def render_if(condition, template, record)
        render template, record if condition 
     end
+
+    def day_month_year(datetime)
+        datetime.strftime('%e, %B, %Y') 
+    end
 end
--- app/helpers/articles_helper.rb
+++
@@ -1,5 +0,0 @@
-module ArticlesHelper
-    def day_month_year(datetime)
-        datetime.strftime('%e, %B, %Y') 
-    end
-end
